e593a88c0a39995fc9516765f3f4002e4b32179d
nick.durbin <nick.durbin@gmail.com>
2020-09-24 20:11:13 -0400

    Setup some db options with postgres

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,33 @@
-import "reflect-metadata"
-import { ApolloServer } from "apollo-server-express";
-import * as Express from "express";
-import {buildSchema, Query, Resolver} from 'type-graphql';
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import * as Express from 'express';
+import { buildSchema, Query, Resolver } from 'type-graphql';
+import { createConnection } from 'typeorm';
 
 @Resolver()
 class HelloResolver {
   @Query(() => String)
   async hello() {
-    return "Hello World";
+    return 'Hello World';
   }
 }
 
 const main = async () => {
+  await createConnection();
+
   const schema = await buildSchema({
     resolvers: [HelloResolver],
   });
 
-  const apolloServer = new ApolloServer({schema});
+  const apolloServer = new ApolloServer({ schema });
 
-  const app = Express()
+  const app = Express();
 
-  apolloServer.applyMiddleware({ app })
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
-    console.log("Server started on port http://localhost:4000/graphql.")
-  })
-
-}
+    console.log('Server started on port http://localhost:4000/graphql.');
+  });
+};
 
 main();
