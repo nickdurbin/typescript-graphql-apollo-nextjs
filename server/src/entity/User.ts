1c9ba2a2a87acf014a57f1aea90e6e5efed3b53f
durbcodes <nick.durbin@gmail.com>
2020-09-25 05:44:39 -0400

    Added a type to the user to be an object with varying fields.

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,16 +1,22 @@
+import { Field, ObjectType, ID } from 'type-graphql';
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 
+@ObjectType()
 @Entity()
 export class User extends BaseEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field()
   @Column()
   firstName: string;
 
+  @Field()
   @Column()
   lastName: string;
 
+  @Field()
   @Column('text', { unique: true })
   email: string;
 
